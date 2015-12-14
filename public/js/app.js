f1dfd01a8754c840f4f9c6b248f00ffdf30981e9
Noah Wimmer <nwimmer123@yahoo.com>
2015-12-13 22:15:58 -0800

    handlebars is halfway to working

--- public/js/app.js
+++ public/js/app.js
@@ -4,26 +4,49 @@ $(document).ready(function() {
 
 	var baseUrl = '/api/books';
 
+//attempt at switching to handlebars
+
+	var allBooks = [];
+
+	var $booksList = $('#books-list');
+
+	var $createBook = $('#create-book');
+
+	var source = $('#books-template').html();
+	var template = Handlebars.compile(source);
+
+	var render = function() {
+		$('#books-list').empty();
+		var booksHtml = template({ books: allBooks });
+		$('#books-list').append(booksHtml);
+	};
+
+	$.get(baseUrl, function (data) {
+		console.log(data);
+		allBooks = data.books;
+		render();
+	});
+
 	//showing books on the homepage
 	//i want these to append to a distinct form each time, so when I go to capture 
 	//a specific book later, there is something to refence later as the selector
-	$.ajax({
-		method: 'GET',
-		url: baseUrl,
-		success: function showBooks (data) {
-			data.forEach(function (element) {
-				//debugger;
-				console.log(element._id);
-				$(".bookBox").append('<li class="list-group-item book" data-id="' + element._id + '">');
-				$(".bookBox").append("<br><p>" + "<b>Contributor:  </b>" + element.contributor + "</p>");
-				$(".bookBox").append("<p>" + "<b>Title:  </b>" + element.title + "</p>");
-				$(".bookBox").append("<p>" + "<b>Author:  </b>" + element.author + "</p>");
-				$(".bookBox").append("<p>" + "<b>Genre:  </b>" + element.genre + "</p><br>");
-				$(".bookBox").append("</div>");
-			});
-		}
+	// $.ajax({
+	// 	method: 'GET',
+	// 	url: baseUrl,
+	// 	success: function showBooks (data) {
+	// 		data.forEach(function (element) {
+	// 			//debugger;
+	// 			console.log(element._id);
+	// 			$(".bookBox").append('<li class="list-group-item book" data-id="' + element._id + '">');
+	// 			$(".bookBox").append("<br><p>" + "<b>Contributor:  </b>" + element.contributor + "</p>");
+	// 			$(".bookBox").append("<p>" + "<b>Title:  </b>" + element.title + "</p>");
+	// 			$(".bookBox").append("<p>" + "<b>Author:  </b>" + element.author + "</p>");
+	// 			$(".bookBox").append("<p>" + "<b>Genre:  </b>" + element.genre + "</p><br>");
+	// 			$(".bookBox").append("</div>");
+	// 		});
+	// 	}
 
-	});
+	// });
 
 	//hide add book field
 	$("#createBook").hide();
