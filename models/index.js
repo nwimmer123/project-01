37d4adcf3f3a009d2f5231e2ab2ce6496c23f133
Noah Wimmer <nwimmer123@yahoo.com>
2015-12-17 11:46:35 -0800

    heroku told me too

--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/project-01_test");
 
+//heroku connection
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  "mongodb://localhost/project-01_test" );
+
 var Book = require('./book');
 var User = require('./user');
 var Review = require('./review');
