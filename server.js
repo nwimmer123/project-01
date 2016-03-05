05bd5ded3d520dc55476cdc9d92abd6b2e6889c9
Noah Wimmer <nwimmer123@yahoo.com>
2016-03-05 12:28:15 -0800

    added login route

--- server.js
+++ server.js
@@ -44,6 +44,49 @@ var Review = require('./models/review');
  * ROUTES *
  **********/
 
+
+// EXPERIMENTAL !!!
+
+ var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+          return res.render("register", {info: "Sorry. That username already exists. Try again."});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
+
+// END EXPERIMENTAL
+
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
